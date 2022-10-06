57a84cb59e59789131e7e448c967fc59d68ea7e4
Anton Chudinovskikh <anton@ennabl.com>
2022-10-06 12:35:22 +0100

    bash/powershell scripts to run jupyter in a conda env

---
+++ jupyter/run-conda.ps1
@@ -0,0 +1,17 @@
+# Activate conda in powershell
+conda init powershell
+
+# Create and Activate Conda Env
+echo "Creating ennablData conda enviornment"
+conda create --name ennablData python=3.9 ipykernel jupyter nb_conda -y
+ 
+echo "Activating ennablData"
+conda activate ennablData
+ 
+# Install Glue Sessions to Env
+echo "Installing dependencies"
+pip install pandas
+pip install sqlalchemy==1.4.32
+pip install pyathena==2.14.0
+
+jupyter notebook
---
+++ jupyter/run-conda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# Create and Activate Conda Env
+echo "Creating ennablData conda enviornment"
+conda create --name ennablData python=3.9 ipykernel jupyter nb_conda -y
+ 
+echo "Activating ennablData"
+eval "$(conda shell.bash hook)"
+conda activate ennablData
+ 
+# Install Glue Sessions to Env
+echo "Installing dependencies"
+pip install pandas
+pip install sqlalchemy==1.4.32
+pip install pyathena==2.14.0
+
+jupyter notebook
